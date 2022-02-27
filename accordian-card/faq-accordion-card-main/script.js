eca6055d76358d683dd2db6e2fbbdbbb5e04eb62
Sangbin <mosangbin94@gmail.com>
2022-02-27 14:03:56 +0900

    acoordion: final

--- accordian-card/faq-accordion-card-main/script.js
+++ accordian-card/faq-accordion-card-main/script.js
@@ -1,16 +1,10 @@
 'use strict';
 
-this.addEventListener("DOMContentLoaded", () =>{
-    const questions = document.querySelectorAll(".question");
-    questions.forEach((question) => question.addEventListener("click", () =>{
-  
-        if(question.parentNode.classList.contains("active")){
-            question.parentNode.classList.toggle("active");
-        }
-    else{
-    questions.forEach(question => question.parentNode.classList.remove("active"));
-    question.parentNode.classList.add("active");
-        }
 
-    }));
-}) 
+const questions = document.querySelectorAll('.question-answer__accordion'); 
+
+questions.forEach((question) => 
+    question.addEventListener ("click", () => { 
+        question.classList.toggle('active');
+    })
+)
