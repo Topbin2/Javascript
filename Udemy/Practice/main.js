94f44047f80669a950152dbb25a9525a2b025663
Sangbin <mosangbin94@gmail.com>
2022-03-26 19:04:32 +0900

    fetch

---
+++ Udemy/Practice/main.js
@@ -0,0 +1,36 @@
+// var requestOptions = {
+//   method: "GET",
+//   redirect: "follow",
+// };
+
+// fetch(
+//   "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDhEeHaM-lrpgIYTkseTk-eiB9kp9X4-qg"
+// )
+//   .then((response) => response.json())
+//   .then((result) => console.log(result.items[0].snippet.title))
+//   .catch((error) => console.log("error", error));
+
+// async function fetchYoutube() {
+//   try {
+//     const res = await fetch(
+//       "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDhEeHaM-lrpgIYTkseTk-eiB9kp9X4-qg"
+//     );
+//     const result = await res.json();
+//     console.log(result);
+//   } catch (e) {
+//     console.log('error!');
+//   }
+// }
+
+// fetchYoutube();
+
+// axios.get( "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDhEeHaM-lrpgIYTkseTk-eiB9kp9X4-qg")
+// .then( res => console.log(res.data) )
+// .catch( err => console.log('err') )
+
+async function fetchApi() {
+    const response = await axios.get("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDhEeHaM-lrpgIYTkseTk-eiB9kp9X4-qg");
+    console.log(response.data);
+}
+
+fetchApi();
