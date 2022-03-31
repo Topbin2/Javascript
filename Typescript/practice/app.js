ceb265083660bf5672ae86ec33290f894ce42572
SANGBIN <mosangbin94@gmail.com>
2022-04-01 02:01:12 +0900

    typescript basic

---
+++ Typescript/practice/app.js
@@ -0,0 +1,40 @@
+// const person: {
+//     name: string;
+//     age: number;
+// }  = {
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string];
+// } = {
+//     name: 'sangbin',
+//     age: 29,
+//     hobbies : ['Sports', 'Cooking'],
+//     role: [2, 'author'],
+// }
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+;
+var person = {
+    name: 'sangbin',
+    age: 29,
+    hobbies: ['Sports', 'Cooking'],
+    role: Role.ADMIN
+};
+// person.role.push('admin');
+// console.log(person.role);
+var favoriteActivities;
+favoriteActivities = ['Sports'];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
+if (person.role === Role.ADMIN) {
+    console.log('enum');
+}
