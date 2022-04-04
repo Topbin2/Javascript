c64ea88c2309417bf762c39abd80134f6212249e
SANGBIN <mosangbin94@gmail.com>
2022-04-04 19:29:06 +0900

    typescript

--- Typescript/drag&drop/src/app.ts
+++
@@ -1,9 +0,0 @@
-class ProjectInput {
-    templateElement: HTMLTemplateElement;
-    hostElement: HTMLDivElement;
-
-    constructor() {
-        this.templateElement = document.querySelector('project-input')! as HTMLTemplateElement;
-        this.hostElement = document.querySelector('app')! as HTMLDivElement;
-    }
-}
