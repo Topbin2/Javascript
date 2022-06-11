89ec3ef372e810de0a839365cd964d2a95eaa55e
MoSangBin <mosangbin94@gmail.com>
2022-06-11 17:52:19 +0900

    ejs

--- Udemy/node/Express/Templating/index.js
+++ Udemy/node/Express/Templating/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const redditData = require("./data.json");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
@@ -21,7 +22,10 @@ app.get("/random", (req, res) => {
 
 app.get("/r/:subreddit", (req, res) => {
   const { subreddit } = req.params;
-  res.render("subreddit", { subreddit });
+  const data = redditData[subreddit];
+
+  if (data) res.render("subreddit", { ...data });
+  else res.render("notfound", { subreddit });
 });
 
 app.listen(3000, (req, res) => {
