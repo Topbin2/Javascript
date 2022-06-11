192bc98625a733a3b5fbb5bdcf528b91cbad1e2c
MoSangBin <mosangbin94@gmail.com>
2022-06-11 16:47:15 +0900

    ejs

--- Udemy/node/Express/FirstApp/index.js
+++ Udemy/node/Express/FirstApp/index.js
@@ -1,68 +1,34 @@
-// const express = require("express");
-// const app = express();
-
-// app.get("/", (req, res) => {
-//   res.send("홈페이지 입니다 !");
-// });
-
-// app.get("/r/:subreddit", (req, res) => {
-//   const { subreddit } = req.params;
-//   res.send(`<h1>Browsing the ${subreddit} subreddit`);
-// });
-
-// app.get("/r/:subreddit/:postId", (req, res) => {
-//   const { subreddit, postId } = req.params;
-//   res.send(`<h1>Browsing the ${subreddit} subreddit, ${postId} postId`);
-// });
-
-// app.post("/cats", (req, res) => {
-//   res.send("cats post request!!?");
-// });
-
-// app.get("/cats", (req, res) => {
-//   res.send("MEOW!!");
-// });
-
-// app.get("/dogs", (req, res) => {
-//   res.send("WOOF!");
-// });
-
-// app.get("/search", (req, res) => {
-//   const { q } = req.query;
-//   if (!q) {
-//     res.send("검색 결과가 없습니다.");
-//   } else {
-//     res.send(`<h1>검색 결과: ${q}</h1>`);
-//   }
-// });
-
-// app.get("*", (req, res) => {
-//   res.send(`i don't know that path!`);
-// });
-
-// app.listen(3000, () => {
-//   console.log("LISTENING ON PORT 3000!");
-// });
-
 const express = require("express");
 const app = express();
 
-app.listen(3000, (req, res) => {
-  console.log("서버 오픈");
-});
-
 app.get("/", (req, res) => {
-  res.send("홈페이지!");
+  res.send("홈페이지 입니다 !");
 });
 
-app.get("/dog/:something", (req, res) => {
-  const { something } = req.params;
-  res.send(`PARAMS: ${something}`);
+app.get("/r/:subreddit", (req, res) => {
+  const { subreddit } = req.params;
+  res.send(`<h1>Browsing the ${subreddit} subreddit`);
+});
+
+app.get("/r/:subreddit/:postId", (req, res) => {
+  const { subreddit, postId } = req.params;
+  res.send(`<h1>Browsing the ${subreddit} subreddit, ${postId} postId`);
+});
+
+app.post("/cats", (req, res) => {
+  res.send("cats post request");
+});
+
+app.get("/cats", (req, res) => {
+  res.send("MEOW!!");
+});
+
+app.get("/dogs", (req, res) => {
+  res.send("WOOF!");
 });
 
 app.get("/search", (req, res) => {
   const { q } = req.query;
-
   if (!q) {
     res.send("검색 결과가 없습니다.");
   } else {
@@ -71,5 +37,39 @@ app.get("/search", (req, res) => {
 });
 
 app.get("*", (req, res) => {
-  res.send(`<h1>잘못된 요청 입니다.</h1>`);
+  res.send(`i don't know that path!`);
 });
+
+app.listen(3000, () => {
+  console.log("LISTENING ON PORT 3000!");
+});
+
+// const express = require("express");
+// const app = express();
+
+// app.listen(3000, (req, res) => {
+//   console.log("서버 오픈");
+// });
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World");
+// });
+
+// app.get("/dog/:something", (req, res) => {
+//   const { something } = req.params;
+//   res.send(`PARAMS: ${something}`);
+// });
+
+// app.get("/search", (req, res) => {
+//   const { q } = req.query;
+
+//   if (!q) {
+//     res.send("검색 결과가 없습니다.");
+//   } else {
+//     res.send(`<h1>검색 결과: ${q}</h1>`);
+//   }
+// });
+
+// app.get("*", (req, res) => {
+//   res.send(`<h1>잘못된 요청 입니다.</h1>`);
+// });
